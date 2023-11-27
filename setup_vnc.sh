27d2fffa9c87de21fc372648ef9fbe18b42a5642
W r e n c h <134978254+Jeanpseven@users.noreply.github.com>
2023-11-26 21:07:11 -0300

    Update setup_vnc.sh

--- setup_vnc.sh
+++ setup_vnc.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+!/bin/bash
 
 # Passo 1: Instalação dos pacotes novnc e x11vnc
 sudo apt-get install novnc x11vnc
@@ -8,4 +8,10 @@ x11vnc -display :0 -autoport -nopwd -bg -xkb -ncache off -quiet -forever &
 
 # Passo 3: Redirecionar a porta 5900 para a porta 80 usando o script launch.sh do novnc
 cd /usr/share/novnc/utils/
-./launch.sh --listen 80 --vnc localhost:5900
+./launch.sh --listen 80 --vnc localhost:5900 &
+
+# Obter o IP local usando curl ifconfig.me
+local_ip=$(curl -s ifconfig.me)
+
+# Exibir o link para acessar o VNC pela web
+echo "Acesse o VNC pela web em: http://${local_ip}/vnc.html"
